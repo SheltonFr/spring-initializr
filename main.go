713f18473faed9c0b5015452f200453a77566727
Shelton Francisco <ssalicuchepa@technoplus.co.mz>
2024-03-26 10:24:56 +0200

    feat: add cobra dependency

--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
+import "spring-initializr/cmd"
 
 func main() {
-	result, err := http.Get("https://start.spring.io/metadata/client")
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer result.Body.Close()
-
-	file, err := os.Create("registries.json")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, result.Body)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Done!!!")
+	cmd.Execute()
 }
